8f3d2607533b79c05c8eec50f28ab31c08b04f55
j.markert <j.markert@8702b9e1-f120-4550-9e2d-81534e70d3e5>
2016-10-26 12:57:15 +0000

    sync missing file
    git-svn-id: https://subversion.gsi.de/hades/fwdetsvn/hydra2@12 8702b9e1-f120-4550-9e2d-81534e70d3e5

--- fwdet/hfwdetstrawvector.h
+++ fwdet/hfwdetstrawvector.h
@@ -21,39 +21,39 @@ public:
     /** Destructor **/
     virtual ~HFwDetStrawVector();
 
-    Int_t GetFlag() const { return fFlag; }
-    Int_t GetNofHits() const { return fNhits; }
-    //Int_t GetNDF() const { return (fNhits > 5) ? fNhits - 4 : 1; }
-    Int_t GetNDF() const { return fNDF; }
-    Double_t GetChi2() const { return fChi2; }
-    Double_t GetX() const { return fParams[0]; }
-    Double_t GetY() const { return fParams[1]; }
-    Double_t GetTx() const { return fParams[2]; }
-    Double_t GetTy() const { return fParams[3]; }
-    Double_t GetZ() const { return fZ; }
-    Double_t GetZH() { return HadesParam(0); }
-    Double_t GetRH() { return HadesParam(1); }
-    Double_t GetThetaH() { return HadesParam(2); }
-    Double_t GetPhiH() { return HadesParam(3); }
-    void HadesParams(Double_t *params);
+    Int_t getFlag() const { return fFlag; }
+    Int_t getNofHits() const { return fNhits; }
+    //Int_t getNDF() const { return (fNhits > 5) ? fNhits - 4 : 1; }
+    Int_t getNDF() const { return fNDF; }
+    Double_t getChi2() const { return fChi2; }
+    Double_t getX() const { return fParams[0]; }
+    Double_t getY() const { return fParams[1]; }
+    Double_t getTx() const { return fParams[2]; }
+    Double_t getTy() const { return fParams[3]; }
+    Double_t getZ() const { return fZ; }
+    Double_t getZH() { return hadesParam(0); }
+    Double_t getRH() { return hadesParam(1); }
+    Double_t getThetaH() { return hadesParam(2); }
+    Double_t getPhiH() { return hadesParam(3); }
+    void hadesParams(Double_t *params);
 
-    Int_t GetHitIndex(Int_t ihit) const { return fHitInds[ihit]; }
-    TBits &GetLR() { return fLRbits; }
-    void Params(Double_t *pars) const { for (Int_t i = 0; i < 4; ++i) pars[i] = fParams[i]; }
-    TMatrixDSym GetCovarMatr();
+    Int_t getHitIndex(Int_t ihit) const { return fHitInds[ihit]; }
+    TBits &getLR() { return fLRbits; }
+    void params(Double_t *pars) const { for (Int_t i = 0; i < 4; ++i) pars[i] = fParams[i]; }
+    TMatrixDSym getCovarMatr();
 
-    void SetFlag(Int_t flag) { fFlag = flag; }
-    void SetNDF(Int_t ndf) { fNDF = ndf; }
-    void SetChi2(Double_t c2) { fChi2 = c2; }
-    void SetZ(Double_t z) { fZ = z; }
-    void SetLRbit(Int_t indx) { fLRbits.SetBitNumber(indx); }
+    void setFlag(Int_t flag) { fFlag = flag; }
+    void setNDF(Int_t ndf) { fNDF = ndf; }
+    void setChi2(Double_t c2) { fChi2 = c2; }
+    void setZ(Double_t z) { fZ = z; }
+    void setLRbit(Int_t indx) { fLRbits.SetBitNumber(indx); }
 
-    Int_t AddHit(Int_t indx);
-    void SetCovar(TMatrixDSym matr);
-    void SetParams(Double_t *pars) { for (Int_t i = 0; i < 4; ++i) fParams[i] = pars[i]; }
+    Int_t addHit(Int_t indx);
+    void setCovar(TMatrixDSym matr);
+    void setParams(Double_t *pars) { for (Int_t i = 0; i < 4; ++i) fParams[i] = pars[i]; }
 
 private:
-    Double_t HadesParam(Int_t ipar);
+    Double_t hadesParam(Int_t ipar);
 
 private:
     Int_t fFlag;              // flag
